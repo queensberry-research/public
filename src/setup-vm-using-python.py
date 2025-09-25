5d35ae326f1a31079381ca5e348ac05d27f78d90
Derek Wan <d.wan@icloud.com>
2025-09-25 20:56:02 +0900

    2025-09-25 20:56:01 (Thu) > DW-Mac > derekwan

--- src/setup-vm-using-python.py
+++ src/setup-vm-using-python.py
@@ -3,13 +3,17 @@ from argparse import ArgumentParser
 from dataclasses import dataclass
 from enum import Enum, auto
 from logging import basicConfig, getLogger
-from os import environ
+from os import environ, geteuid
 from pathlib import Path
 from re import search
 from shutil import which
+from subprocess import check_call
+from tempfile import TemporaryDirectory
 from typing import TextIO
+from urllib.request import urlopen
 
 _LOGGER = getLogger(__name__)
+_BOTTOM_VERSION = "0.11.1"
 basicConfig(
     format="{asctime} | {message}", datefmt="%Y-%m-%d %H:%M:%S", style="{", level="INFO"
 )
@@ -21,6 +25,7 @@ basicConfig(
 class Settings:
     aliases: bool = False
     bottom: bool = False
+    bottom_version: str = _BOTTOM_VERSION
 
 
 class Shell(Enum):
@@ -51,7 +56,7 @@ def main(settings: Settings, /) -> None:
     if settings.aliases:
         _setup_aliases()
     if settings.bottom:
-        _setup_bottom()
+        _setup_bottom(version=settings.bottom_version)
 
 
 def _setup_aliases() -> None:
@@ -82,10 +87,22 @@ alias l='ls -al'
                 append(fh)
 
 
-def _setup_bottom() -> None:
+def _setup_bottom(*, version: str = _BOTTOM_VERSION) -> None:
     if _has_command("btm"):
         _LOGGER.info("'bottom' is already set up")
         return
+    filename = f"bottom_{version}-1_amd64.deb"
+    url = (
+        f"https://github.com/ClementTsang/bottom/releases/download/{version}/{filename}"
+    )
+    with TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir, filename)
+        with urlopen(url) as response, temp_file.open(mode="wb") as fh:  # noqa: S310
+            fh.write(response.read())
+        cmd = ["dpkg", "-i", str(temp_file)]
+        if not _is_root():
+            cmd = ["sudo", *cmd]
+        check_call(cmd)  # noqa: S603
 
 
 # utilities
@@ -95,16 +112,30 @@ def _has_command(cmd: str, /) -> bool:
     return which(cmd) is not None
 
 
+def _is_root() -> bool:
+    return geteuid() == 0
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
+        "-a",
         "--aliases",
         dest="aliases",
         action="store_true",
         help="Add aliases (default: disabled)",
     )
     parser.add_argument(
-        "--bottom", dest="bottom", action="store_true", help="Install bottom"
+        "-b",
+        "--bottom",
+        dest="bottom",
+        action="store_true",
+        help="Install bottom (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--bottom-version",
+        default=_BOTTOM_VERSION,
+        help="Bottom version (default: %(default)s)",
     )
     args = parser.parse_args()
     settings = Settings(aliases=args.aliases, bottom=args.bottom)
