3fdcf54b4897aeab1c7ba37012886c057271b4b2
Derek Wan <d.wan@icloud.com>
2025-09-26 00:15:22 +0900

    Add `setup-vm-using-python` (#29)

---
+++ src/setup-vm-using-python.py
@@ -0,0 +1,569 @@
+#!/usr/bin/env python3.13
+import tarfile
+from argparse import ArgumentParser
+from collections.abc import Iterator
+from contextlib import contextmanager
+from dataclasses import dataclass
+from enum import Enum, auto
+from logging import basicConfig, getLogger
+from os import environ, geteuid
+from pathlib import Path
+from shutil import copyfile, copytree, rmtree, which
+from subprocess import check_call
+from tempfile import TemporaryDirectory
+from typing import assert_never
+from urllib.parse import urlparse
+from urllib.request import urlopen
+
+_LOGGER = getLogger(__name__)
+_BOTTOM_VERSION = "0.11.1"
+_DELTA_VERSION = "0.18.2"
+_DIRENV_VERSION = "2.37.1"
+_JUST_VERSION = "1.42.4"
+_NEOVIM_VERSION = "0.11.4"
+_STARSHIP_VERSION = "1.23.0"
+_UV_VERSION = "0.8.22"
+_PATH_LOCAL_BIN = Path.home().joinpath(".local", "bin")
+_PATH_LOCAL_BIN.mkdir(parents=True, exist_ok=True)
+basicConfig(
+    format="{asctime} | {message}", datefmt="%Y-%m-%d %H:%M:%S", style="{", level="INFO"
+)
+
+
+# classes
+
+
+@dataclass(order=True, unsafe_hash=True, kw_only=True, slots=True)
+class Settings:
+    # bottom
+    bottom: bool = False
+    bottom_force: bool = False
+    bottom_version: str = _BOTTOM_VERSION
+    # delta
+    delta: bool = False
+    delta_force: bool = False
+    delta_version: str = _DELTA_VERSION
+    # direnv
+    direnv: bool = False
+    direnv_force: bool = False
+    direnv_version: str = _DIRENV_VERSION
+    # git
+    git: bool = False
+    git_force: bool = False
+    # just
+    just: bool = False
+    just_force: bool = False
+    just_version: str = _JUST_VERSION
+    # lazyvim
+    lazyvim: bool = False
+    lazyvim_force: bool = False
+    # neovim
+    neovim: bool = False
+    neovim_force: bool = False
+    neovim_version: str = _NEOVIM_VERSION
+    # proxmox
+    proxmox_apt: bool = False
+    # starship
+    starship: bool = False
+    starship_force: bool = False
+    starship_version: str = _STARSHIP_VERSION
+    # uv
+    uv: bool = False
+    uv_force: bool = False
+    uv_version: str = _UV_VERSION
+    # vim
+    vim: bool = False
+    vim_force: bool = False
+
+    @property
+    def git_use(self) -> bool:
+        return self.git or self.lazyvim
+
+    @property
+    def neovim_use(self) -> bool:
+        return self.neovim or self.lazyvim
+
+    @property
+    def proxmox_apt_use(self) -> bool:
+        return self.proxmox_apt or self.git_use or self.vim
+
+
+class Shell(Enum):
+    bash = auto()
+    zsh = auto()
+
+    @classmethod
+    def get(cls) -> "Shell":
+        match Path(environ["SHELL"]).name:
+            case "bash":
+                return Shell.bash
+            case "zsh":
+                return Shell.zsh
+            case shell:
+                msg = f"Invalid shell: {shell!r}"
+                raise ValueError(msg)
+
+    @property
+    def path_rc(self) -> Path:
+        return Path.home().joinpath(f".{self.name}rc")
+
+
+# main
+
+
+def main(settings: Settings, /) -> None:
+    _LOGGER.info("Setting up VM...")
+    _setup_aliases()
+    _setup_editing_mode()
+    _setup_env_vars()
+    if settings.proxmox_apt_use:
+        _setup_proxmox_apt()
+    if settings.git_use:  # after proxmox_apt
+        _setup_git(force=settings.git_force)
+    if settings.vim:  # after proxmox_apt
+        _setup_vim(force=settings.vim_force)
+    if settings.neovim_use:
+        _setup_neovim(force=settings.neovim_force, version=settings.neovim_version)
+    if settings.lazyvim:  # after neovim
+        _setup_lazyvim(force=settings.lazyvim_force)
+    if settings.bottom:
+        _setup_bottom(force=settings.bottom_force, version=settings.bottom_version)
+    if settings.delta:
+        _setup_delta(force=settings.delta_force, version=settings.delta_version)
+    if settings.direnv:
+        _setup_direnv(force=settings.direnv_force, version=settings.direnv_version)
+    if settings.just:
+        _setup_just(force=settings.just_force)
+    if settings.starship:
+        _setup_starship(
+            force=settings.starship_force, version=settings.starship_version
+        )
+    if settings.uv:
+        _setup_uv(force=settings.uv_force, version=settings.uv_version)
+
+
+def _setup_aliases() -> None:
+    for line in [
+        """alias ~='cd "${HOME}"'""",
+        """alias ..='cd ..'""",
+        """alias ...='cd ../..'""",
+        """alias ....='cd ../../..'""",
+        """alias l='ls -al'""",
+    ]:
+        _append_to_rc(line)
+
+
+def _setup_bottom(*, force: bool = False, version: str = _BOTTOM_VERSION) -> None:
+    if _has_command("btm") and not force:
+        _LOGGER.info("'bottom' is already set up")
+        return
+    _LOGGER.info("Setting up 'bottom' %s...", version)
+    url = _github_url(
+        "ClementTsang", "bottom", version, f"bottom_{version}-1_amd64.deb"
+    )
+    with _yield_download(url) as temp_file:
+        cmd = ["dpkg", "-i", str(temp_file)]
+        check_call(_prepend_sudo_if_not_root(cmd))
+
+
+def _setup_delta(*, force: bool = False, version: str = _DELTA_VERSION) -> None:
+    if _has_command("delta") and not force:
+        _LOGGER.info("'delta' is already set up")
+        return
+    _LOGGER.info("Setting up 'delta' %s...", version)
+    url = _github_url(
+        "dandavison",
+        "delta",
+        version,
+        f"delta-{version}-x86_64-unknown-linux-gnu.tar.gz",
+    )
+    with (
+        _yield_download(url) as temp_file,
+        _yield_tar_gz_contents(temp_file) as temp_dir,
+    ):
+        (dir_from,) = temp_dir.iterdir()
+        path_from = dir_from.joinpath("delta")
+        path_to = _PATH_LOCAL_BIN.joinpath("delta")
+        _copyfile_logged(path_from, path_to)
+
+
+def _setup_direnv(*, force: bool = False, version: str = _DIRENV_VERSION) -> None:
+    if _has_command("direnv") and not force:
+        _LOGGER.info("'direnv' is already set up")
+        return
+    _LOGGER.info("Setting up 'direnv'...")
+    url = _github_url("direnv", "direnv", f"v{version}", "direnv.linux-amd64")
+    with _yield_download(url) as temp_file:
+        path_to = _PATH_LOCAL_BIN.joinpath("direnv")
+        _copyfile_logged(temp_file, path_to)
+    _append_to_rc(f"""eval "$(direnv hook {Shell.get().name})" """)
+
+
+def _setup_editing_mode() -> None:
+    match Shell.get():
+        case Shell.bash:
+            line = "set -o vi"
+        case Shell.zsh:
+            line = "bindkey -v"
+        case never:
+            assert_never(never)
+    _append_to_rc(line)
+
+
+def _setup_env_vars() -> None:
+    _append_to_rc("""export EDITOR='nvim'""")
+    _append_to_rc('''export PATH="${HOME}/.local/bin${PATH:+:${PATH}}"''')
+
+
+def _setup_git(*, force: bool = False) -> None:
+    _setup_via_apt("git", force=force)
+
+
+def _setup_just(*, force: bool = False, version: str = _JUST_VERSION) -> None:
+    if _has_command("just") and not force:
+        _LOGGER.info("'just' is already set up")
+        return
+    _LOGGER.info("Setting up 'just'...")
+    url = _github_url(
+        "casey", "just", version, f"just-{version}-x86_64-unknown-linux-musl.tar.gz"
+    )
+    with (
+        _yield_download(url) as temp_file,
+        _yield_tar_gz_contents(temp_file) as temp_dir,
+    ):
+        path_from = temp_dir.joinpath("just")
+        path_to = _PATH_LOCAL_BIN.joinpath("just")
+        _copyfile_logged(path_from, path_to)
+
+
+def _setup_lazyvim(*, force: bool = False) -> None:
+    home = Path.home()
+    nvim = home.joinpath(".config", "nvim")
+    lua = nvim.joinpath("lua")
+    lazy = lua.joinpath("config", "lazy.lua")
+    if lazy.exists() and not force:
+        _LOGGER.info("'lazyvim' is already set up")
+        return
+    _LOGGER.info("Setting up 'lazyvim'...")
+    for tail in [
+        Path(".config", "nvim"),
+        Path(".local", "share"),
+        Path(".local", "state"),
+        Path(".cache", "nvim"),
+    ]:
+        _unlink_logged(home.joinpath(tail))
+    check_call(["git", "clone", "https://github.com/LazyVim/starter", str(nvim)])
+    path = lua.joinpath("plugins", "auto-save.lua")
+    _unlink_logged(path)
+    _LOGGER.info("Writing %r...", str(path))
+    path.write_text("""
+return { "pocco81/auto-save.nvim }
+""")
+
+
+def _setup_neovim(*, force: bool = False, version: str = _NEOVIM_VERSION) -> None:
+    if _has_command("nvim") and not force:
+        _LOGGER.info("'neovim' is already set up")
+        return
+    _LOGGER.info("Setting up 'neovim'...")
+    stem = "nvim-linux-x86_64"
+    url = _github_url("neovim", "neovim", f"v{version}", f"{stem}.tar.gz")
+    with (
+        _yield_download(url) as temp_file,
+        _yield_tar_gz_contents(temp_file) as temp_dir,
+    ):
+        (path_from,) = temp_dir.iterdir()
+        path_to = _PATH_LOCAL_BIN.joinpath("neovim", stem)
+        _copytree_logged(path_from, path_to)
+    path_from = _PATH_LOCAL_BIN.joinpath("nvim")
+    _unlink_logged(path_from)
+    path_to = _PATH_LOCAL_BIN.joinpath("neovim", stem, "bin", "nvim")
+    path_from.symlink_to(path_to)
+
+
+def _setup_proxmox_apt() -> None:
+    any_removed = any(
+        _setup_proxmox_apt_remove(name) for name in ["ceph", "pve-enterprise"]
+    )
+    if any_removed:
+        _update_apt()
+
+
+def _setup_proxmox_apt_remove(name: str, /) -> bool:
+    path = Path("/etc", "apt", "sources.list.d", f"{name}.sources")
+    if path.exists():
+        _unlink_logged(path)
+        return True
+    _LOGGER.info("%r is already removed", str(path))
+    return False
+
+
+def _setup_starship(*, force: bool = False, version: str = _STARSHIP_VERSION) -> None:
+    if _has_command("starship") and not force:
+        _LOGGER.info("'starship' is already set up")
+        return
+    url = _github_url(
+        "starship",
+        "starship",
+        f"v{version}",
+        "starship-x86_64-unknown-linux-gnu.tar.gz",
+    )
+    with (
+        _yield_download(url) as temp_file,
+        _yield_tar_gz_contents(temp_file) as temp_dir,
+    ):
+        (path_from,) = temp_dir.iterdir()
+        path_to = _PATH_LOCAL_BIN.joinpath("starship")
+        _copyfile_logged(path_from, path_to)
+
+
+def _setup_uv(*, force: bool = False, version: str = _UV_VERSION) -> None:
+    if _has_command("uv") and not force:
+        _LOGGER.info("'uv' is already set up")
+        return
+    url = _github_url("astral-sh", "uv", version, "uv-x86_64-unknown-linux-gnu.tar.gz")
+    with (
+        _yield_download(url) as temp_file,
+        _yield_tar_gz_contents(temp_file) as temp_dir,
+    ):
+        (dir_from,) = temp_dir.iterdir()
+        for name in ["uv", "uvx"]:
+            path_from = dir_from.joinpath(name)
+            path_to = _PATH_LOCAL_BIN.joinpath(name)
+            _copyfile_logged(path_from, path_to)
+
+
+def _setup_vim(*, force: bool = False) -> None:
+    _setup_via_apt("vim", force=force)
+
+
+# utilities
+
+
+def _append_to_rc(line: str, /) -> None:
+    path = Shell.get().path_rc
+    try:
+        lines = path.read_text().splitlines()
+    except FileNotFoundError:
+        _LOGGER.info("Writing %r to %r...", line, str(path))
+        with path.open(mode="w") as fh:
+            fh.write(f"{line}\n")
+    else:
+        if any(line_i == line for line_i in lines):
+            _LOGGER.info("%r already in %r", line, str(path))
+        else:
+            _LOGGER.info("Appending %r to %r...", line, str(path))
+            with path.open(mode="a") as fh:
+                fh.write(f"{line}\n")
+
+
+def _copyfile_logged(path_from: Path, path_to: Path, /) -> None:
+    _unlink_logged(path_to)
+    _LOGGER.info("Copying %r -> %r...", str(path_from), str(path_to))
+    copyfile(path_from, path_to)
+
+
+def _copytree_logged(path_from: Path, path_to: Path, /) -> None:
+    if path_to.exists():
+        _LOGGER.info("Removing %r...", str(path_to))
+        rmtree(path_to)
+    _LOGGER.info("Copying %r -> %r...", str(path_from), str(path_to))
+    copytree(path_from, path_to)
+
+
+def _github_url(owner: str, repo: str, version: str, filename: str, /) -> str:
+    return f"https://github.com/{owner}/{repo}/releases/download/{version}/{filename}"
+
+
+def _has_command(cmd: str, /) -> bool:
+    return which(cmd) is not None
+
+
+def _prepend_sudo_if_not_root(cmd: list[str], /) -> list[str]:
+    return cmd if _is_root() else ["sudo", *cmd]
+
+
+def _is_root() -> bool:
+    return geteuid() == 0
+
+
+def _unlink_logged(path: Path, /) -> None:
+    if path.exists():
+        _LOGGER.info("Removing %r...", str(path))
+        path.unlink(missing_ok=True)
+
+
+def _update_apt() -> None:
+    _LOGGER.info("Updating 'apt'...")
+    cmd = ["apt", "update"]
+    check_call(_prepend_sudo_if_not_root(cmd))
+
+
+def _setup_via_apt(cmd: str, /, *, force: bool = False) -> None:
+    if _has_command(cmd) and not force:
+        _LOGGER.info("%r is already set up", cmd)
+        return
+    _LOGGER.info("Setting up %r...", str(cmd))
+    _update_apt()
+    check_call(_prepend_sudo_if_not_root(["apt", "install", "-y", cmd]))
+
+
+@contextmanager
+def _yield_download(url: str, /) -> Iterator[Path]:
+    filename = Path(urlparse(url).path).name
+    with TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir, filename)
+        with urlopen(url) as response, temp_file.open(mode="wb") as fh:
+            fh.write(response.read())
+        yield temp_file
+
+
+@contextmanager
+def _yield_tar_gz_contents(path: Path, /) -> Iterator[Path]:
+    with tarfile.open(path, "r:gz") as tf, TemporaryDirectory() as temp_dir:
+        _ = tf.extractall(path=temp_dir, filter="data")
+        yield Path(temp_dir)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    # bottom
+    parser.add_argument(
+        "-b",
+        "--bottom",
+        action="store_true",
+        help="Install 'bottom' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--bottom-force",
+        action="store_true",
+        help="Force install 'bottom' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--bottom-version",
+        default=_BOTTOM_VERSION,
+        help="'bottom' version (default: %(default)s)",
+    )
+    # delta
+    parser.add_argument(
+        "--delta", action="store_true", help="Install 'delta' (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--delta-force",
+        action="store_true",
+        help="Force install 'delta' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--delta-version",
+        default=_DELTA_VERSION,
+        help="'delta' version (default: %(default)s)",
+    )
+    # direnv
+    parser.add_argument(
+        "--direnv", action="store_true", help="Install 'direnv' (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--direnv-force",
+        action="store_true",
+        help="Force install 'direnv' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--direnv-version",
+        default=_DIRENV_VERSION,
+        help="'direnv' version (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-g", "--git", action="store_true", help="Install 'git' (default: %(default)s)"
+    )
+    # just
+    parser.add_argument(
+        "-j",
+        "--just",
+        action="store_true",
+        help="Install 'just' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--just-force",
+        action="store_true",
+        help="Force install 'just' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--just-version",
+        default=_JUST_VERSION,
+        help="'just' version (default: %(default)s)",
+    )
+    # lazyvim
+    parser.add_argument(
+        "-l",
+        "--lazyvim",
+        action="store_true",
+        help="Install 'lazyvim' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--lazyvim-force",
+        action="store_true",
+        help="Force install 'lazyvim' (default: %(default)s)",
+    )
+    # neovim
+    parser.add_argument(
+        "-n",
+        "--neovim",
+        action="store_true",
+        help="Install 'neovim' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--neovim-force",
+        action="store_true",
+        help="Force install 'neovim' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--neovim-version",
+        default=_NEOVIM_VERSION,
+        help="'neovim' version (default: %(default)s)",
+    )
+    # proxmox
+    parser.add_argument(
+        "--proxmox-apt",
+        action="store_true",
+        help="Setup proxmox apt (default: %(default)s)",
+    )
+    # starship
+    parser.add_argument(
+        "-s",
+        "--starship",
+        action="store_true",
+        help="Install 'starship' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--starship-force",
+        action="store_true",
+        help="Force install 'starship' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--starship-version",
+        default=_STARSHIP_VERSION,
+        help="'starship' version (default: %(default)s)",
+    )
+    # uv
+    parser.add_argument(
+        "-u", "--uv", action="store_true", help="Install 'uv' (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--uv-force",
+        action="store_true",
+        help="Force install 'uv' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--uv-version", default=_UV_VERSION, help="'uv' version (default: %(default)s)"
+    )
+    # vim
+    parser.add_argument(
+        "-v", "--vim", action="store_true", help="Install 'vim' (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--vim-force",
+        action="store_true",
+        help="Force install 'vim' (default: %(default)s)",
+    )
+    settings = Settings(**vars(parser.parse_args()))
+    main(settings)
