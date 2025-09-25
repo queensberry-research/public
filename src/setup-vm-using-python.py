f2092011a7600a6616091a1ee9faa45e8426f5e6
Derek Wan <d.wan@icloud.com>
2025-09-25 20:32:41 +0900

    2025-09-25 20:32:41 (Thu) > DW-Mac > derekwan

--- src/setup-vm-using-python.py
+++ src/setup-vm-using-python.py
@@ -5,6 +5,9 @@ from enum import Enum, auto
 from logging import basicConfig, getLogger
 from os import environ
 from pathlib import Path
+from re import search
+from shutil import which
+from typing import TextIO
 
 _LOGGER = getLogger(__name__)
 basicConfig(
@@ -17,6 +20,7 @@ basicConfig(
 @dataclass(order=True, unsafe_hash=True, kw_only=True, slots=True)
 class Settings:
     aliases: bool = False
+    bottom: bool = False
 
 
 class Shell(Enum):
@@ -43,24 +47,68 @@ class Shell(Enum):
 
 def main(settings: Settings, /) -> None:
     _LOGGER.info("Setting up VM...")
-
     if settings.aliases:
         _setup_aliases()
+    if settings.bottom:
+        _setup_bottom()
 
 
 def _setup_aliases() -> None:
     _LOGGER.info("Setting up aliases...")
     path = Shell.get().path_rc
 
+    def append(file: TextIO, /) -> None:
+        file.write("""
+
+# aliases
+alias ~='cd "${HOME}"'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias l='ls -al'
+""")
+
+    try:
+        lines = path.read_text().splitlines()
+    except FileNotFoundError:
+        _LOGGER.info("Writing to %r...", str(path))
+        with path.open(mode="w") as fh:
+            append(fh)
+    else:
+        if not any(search("# aliases", line) for line in lines):
+            _LOGGER.info("Appending to %r...", str(path))
+            with path.open(mode="a") as fh:
+                append(fh)
+
+
+def _setup_bottom() -> None:
+    if _has_command("btm"):
+        _LOGGER.info("'bottom' is already set up")
+        return
+    pass
+
+
+# utilities
+
+
+def _has_command(cmd: str, /) -> bool:
+    return which(cmd) is not None
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "--alias",
-        dest="alias",
+        "--aliases",
+        dest="aliases",
         action="store_true",
         help="Add aliases (default: disabled)",
     )
+    parser.add_argument(
+        "--bottom",
+        dest="bottom",
+        action="store_true",
+        help="Install bottom",
+    )
     args = parser.parse_args()
-    settings = Settings(aliases=args.alias)
+    settings = Settings(aliases=args.aliases, bottom=args.bottom)
     main(settings)
