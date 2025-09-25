e0bef8a51628ae5bb4d00dad6b0a9c222ca88e45
Derek Wan <d.wan@icloud.com>
2025-09-25 19:49:42 +0900

    2025-09-25 19:49:41 (Thu) > DW-Mac > derekwan

---
+++ src/setup-vm-using-python.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "click>=8.3,<8.4",
+#     "dycw-utilities>=0.167,<0.168",
+#     "pydantic-settings>=2.11,<2.12",
+#     "requests>=2.32.5,<2.33",
+# ]
+# ///
