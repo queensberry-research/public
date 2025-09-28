1682ce33afe640dee2680275b028817dbac6cdde
Derek Wan <d.wan@icloud.com>
2025-09-28 21:04:05 +0900

    save

--- src/setup-infra.py
+++ src/setup-infra.py
@@ -378,7 +378,8 @@ def setup_docker(*, force: bool = False) -> None:
         "install -m 0755 -d /etc/apt/keyrings",
         "curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
         "chmod a+r /etc/apt/keyrings/docker.asc",
-        """echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null""",
+        """echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian trixie stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null""",
+        # """echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null""",
         "apt-get update",
         "apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
         "usermod -aG docker $USER",
@@ -450,7 +451,6 @@ def setup_neovim(*, force: bool = False, version: str = _NEOVIM_VERSION) -> None
     _unlink_logged(path_from)
     path_to = _local_bin().joinpath("neovim", stem, "bin", "nvim")
     path_from.symlink_to(path_to)
-    _append_to_rc("""export EDITOR='nvim'""")
     _append_to_rc("""alias n='nvim'""")
 
 
@@ -490,6 +490,8 @@ def setup_rc() -> None:
         """alias zshrc='$EDITOR "${HOME}/.zshrc"'""",
     ]:
         _append_to_rc(line)
+    _append_to_rc("""export EDITOR=$(command -v nvim || command -v vim || echo vi)""")
+    _append_to_rc('''export PATH="${HOME}/.local/bin${PATH:+:${PATH}}"''')
     match Shell.get():
         case Shell.bash:
             line = "set -o vi"
@@ -498,7 +500,6 @@ def setup_rc() -> None:
         case never:
             assert_never(never)
     _append_to_rc(line)
-    _append_to_rc('''export PATH="${HOME}/.local/bin${PATH:+:${PATH}}"''')
 
 
 def setup_ssh_keys(*, mode: SSHKeysMode) -> None:
