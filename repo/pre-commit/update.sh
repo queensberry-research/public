a026423fc0e92011d07217480cde4db857cbfb33
Derek Wan <d.wan@icloud.com>
2025-10-11 09:20:47 +0900

    Migrate in `common` (#82)

---
+++ repo/pre-commit/update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+root=$(git rev-parse --show-toplevel)
+filename='.pre-commit-config.yaml'
+root_config="$root/$filename"
+backup_config="$root/$filename.backup"
+repo_config="$root/repo/pre-commit/$filename"
+cp "$root_config" "$backup_config"
+mv "$repo_config" "$root_config"
+pre-commit autoupdate
+mv "$root_config" "$repo_config"
+mv "$backup_config" "$root_config"
