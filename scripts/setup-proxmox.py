98a72876ce4ce7640771729ab694506bc0392f5a
Derek Wan <d.wan@icloud.com>
2025-10-12 11:15:22 +0900

    '2025-10-12 11:15:22 (Sun)  > DW-Mac  > derekwan'

--- scripts/setup-proxmox.py
+++ scripts/setup-proxmox.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
+from subprocess import CalledProcessError, check_call, check_output, run
+
 
 def main() -> None:
-    pass
+    _ = check_call("apt install -y git", shell=True)
 
 
 if __name__ == "__main__":
