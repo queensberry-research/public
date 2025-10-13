60a24e6dbf2a558711612d2b0dffb50f892c6886
Derek Wan <d.wan@icloud.com>
2025-10-13 14:53:31 +0900

    '2025-10-13 14:53:31 (Mon)  > DW-Mac  > derekwan'

--- src/public/constants.py
+++ src/public/constants.py
@@ -1,1 +1,10 @@
-installer/src/installer/constants.py
+from __future__ import annotations
+
+from pathlib import Path
+
+from .installer_constants import HOME, LOCAL_BIN, SSH, XDG_CONFIG_HOME
+
+ETC = Path("/etc")
+
+
+__all__ = ["ETC", "HOME", "LOCAL_BIN", "SSH", "XDG_CONFIG_HOME"]
