b2ab22a52bd2a3af6154198c2058ef320bb26f79
Derek Wan <d.wan@icloud.com>
2025-10-15 08:36:37 +0900

    Remove all extra logging (#154)

--- src/public/install.py
+++ src/public/install.py
@@ -132,7 +132,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.61")
+    _LOGGER.info("'public' version: 0.4.62")
     settings = _Settings.parse()
     match settings.command:
         case "init":
