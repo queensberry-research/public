e17a4f8e1a40642b092fd706bada0fc372a889f2
Derek Wan <d.wan@icloud.com>
2025-10-15 11:51:29 +0900

    Update msg on `uv` missing in `.envrc` (#165)

--- src/public/__init__.py
+++ src/public/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-__version__ = "0.4.72"
+__version__ = "0.4.73"
--- src/public/install.py
+++ src/public/install.py
@@ -128,7 +128,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.72")
+    _LOGGER.info("'public' version: 0.4.73")
     settings = _Settings.parse()
     match settings.command:
         case "init":
