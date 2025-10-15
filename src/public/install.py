6dfbcfc1f08b368a7bc9b457c071bb6f4f3a89d8
Derek Wan <d.wan@icloud.com>
2025-10-15 23:33:50 +0900

    Re-order in SSH config (#176)

--- src/public/install.py
+++ src/public/install.py
@@ -99,7 +99,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.82")
+    _LOGGER.info("'public' version: 0.4.83")
     settings = _Settings.parse()
     match settings.command:
         case "init":
