54c0f08db24502bea54d65a1557b5c2989cc1caa
Derek Wan <d.wan@icloud.com>
2025-10-15 17:40:43 +0900

    Add `repo/ignore` and use `repo/.envrc` (#170)

--- src/public/install.py
+++ src/public/install.py
@@ -128,7 +128,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.77")
+    _LOGGER.info("'public' version: 0.4.78")
     settings = _Settings.parse()
     match settings.command:
         case "init":
