a1bcf312c8f2a7df1f6d3f0718074763331fb3d8
Derek Wan <d.wan@icloud.com>
2025-10-15 22:57:22 +0900

    Have `neovim` sync plugins (#173)

--- src/public/install.py
+++ src/public/install.py
@@ -128,7 +128,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.79")
+    _LOGGER.info("'public' version: 0.4.80")
     settings = _Settings.parse()
     match settings.command:
         case "init":
--- src/public/utilities.py
+++ src/public/utilities.py
@@ -51,7 +51,7 @@ _LOGGER = getLogger(__name__)
 
 
 def log_public_version() -> None:
-    _LOGGER.info("'public' version: 0.4.79")
+    _LOGGER.info("'public' version: 0.4.80")
 
 
 __all__ = [
