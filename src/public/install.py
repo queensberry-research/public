96ec511a789da24ddc0d7af2d54d11bbcb2b67b5
Derek Wan <d.wan@icloud.com>
2025-10-16 11:55:36 +0900

    '2025-10-16 11:55:36 (Thu)  > DW-Mac  > derekwan'

--- src/public/install.py
+++ src/public/install.py
@@ -99,7 +99,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.88")
+    _LOGGER.info("'public' version: 0.4.89")
     settings = _Settings.parse()
     match settings.command:
         case "init":
