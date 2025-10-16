1f2b78a7861e1839546791b6fe8b059f4b8f0077
Derek Wan <d.wan@icloud.com>
2025-10-16 10:11:01 +0900

    Use `taplo` for formatting (edit `repo/pre-commit`) (#178)

--- src/public/install.py
+++ src/public/install.py
@@ -99,7 +99,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.84")
+    _LOGGER.info("'public' version: 0.4.85")
     settings = _Settings.parse()
     match settings.command:
         case "init":
