f0c1b69e0bc9ae2accc5c75b66acfab13e57f112
Derek Wan <d.wan@icloud.com>
2025-10-16 09:40:54 +0900

    Use `taplo` for formatting (#177)

--- src/public/install.py
+++ src/public/install.py
@@ -99,7 +99,7 @@ def _main() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.83")
+    _LOGGER.info("'public' version: 0.4.84")
     settings = _Settings.parse()
     match settings.command:
         case "init":
