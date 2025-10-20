94934f8178524576f7f36fdfadbf44053970e9fb
Derek Wan <d.wan@icloud.com>
2025-10-20 22:25:03 +0900

    '2025-10-20 22:25:03 (Mon)  > DW-Mac  > derekwan'

--- src/public/install.py
+++ src/public/install.py
@@ -101,7 +101,7 @@ def _install() -> None:
         style="{",
         level="INFO",
     )
-    _LOGGER.info("'public' version: 0.4.167")
+    _LOGGER.info("'public' version: 0.4.168")
     settings = _PublicInstallerSettings.parse()
     match settings.mode:
         case None:
