cbb1d43a4df8f8ff77b9a6a468aa401fdf4edbc0
Derek Wan <d.wan@icloud.com>
2025-10-21 14:13:17 +0900

    '2025-10-21 14:13:17 (Tue)  > DW-Mac  > derekwan'

--- src/public/install.py
+++ src/public/install.py
@@ -34,7 +34,7 @@ basicConfig(
 _LOGGER = getLogger(__name__)
 
 
-__version__ = "0.5.12"
+__version__ = "0.5.13"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHON3_M = "python3 -m"
