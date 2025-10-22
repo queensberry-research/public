d5b342abf576e8ebcb0954433bca4405963b30fa
Derek Wan <d.wan@icloud.com>
2025-10-22 22:12:15 +0900

    Update submodules (split `SOURCE_BASHRC`) (#286)

--- src/public/install.py
+++ src/public/install.py
@@ -34,7 +34,7 @@ basicConfig(
 _LOGGER = getLogger(__name__)
 
 
-__version__ = "0.5.17"
+__version__ = "0.5.18"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHON3_M = "python3 -m"
