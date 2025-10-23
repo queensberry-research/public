6e4f6467e0dda312c9fc6433bd40c14af2daf626
Derek Wan <d.wan@icloud.com>
2025-10-23 15:23:37 +0900

    '2025-10-23 15:23:37 (Thu)  > DW-Mac  > derekwan'

--- src/public/install.py
+++ src/public/install.py
@@ -39,7 +39,6 @@ __version__ = "0.5.27"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHON3_M = "python3 -m"
-_PYTHON3_M_PUBLIC = f"{_PYTHON3_M} public.install"
 FLAG_PUBLIC_VERSION = "--public-version"
 FLAG_INSTALLER_VERSION = "--installer-version"
 FLAG_INFRA_VERSION = "--infra-version"
@@ -290,7 +289,8 @@ def _core_install(
     )
     mode: _Mode = "core-in-repo"
     parts: list[str] = [
-        _PYTHON3_M_PUBLIC,
+        _PYTHON3_M,
+        "public.install",
         _FLAG_MODE,
         mode,
         FLAG_PUBLIC_VERSION,
