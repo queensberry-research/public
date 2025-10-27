17c4b2dca441492dcff3e689c37cd880b42ec61a
Derek Wan <d.wan@icloud.com>
2025-10-27 18:17:20 +0900

    Update submodules (#318)

--- src/public/install.py
+++ src/public/install.py
@@ -35,7 +35,7 @@ basicConfig(
 _LOGGER = getLogger(__name__)
 
 
-__version__ = "0.5.49"
+__version__ = "0.5.50"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHON3_M = "python3 -m"
