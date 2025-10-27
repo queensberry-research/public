5e70da8bbab11525864046c47c0a166777afb69a
Derek Wan <d.wan@icloud.com>
2025-10-27 09:20:49 +0900

    Re-format `pyproject.toml` (#311)

--- src/public/install.py
+++ src/public/install.py
@@ -35,7 +35,7 @@ basicConfig(
 _LOGGER = getLogger(__name__)
 
 
-__version__ = "0.5.42"
+__version__ = "0.5.43"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHON3_M = "python3 -m"
