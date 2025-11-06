88ebb834d9f7b07bf833224404c5cf981e1e5f4b
Derek Wan <d.wan@icloud.com>
2025-11-06 10:30:16 +0900

    2025-11-06 10:30:16 (Thu)  > DW-Mac  > derekwan

--- src/public/install.py
+++ src/public/install.py
@@ -34,7 +34,7 @@ basicConfig(
 _LOGGER = getLogger(__name__)
 
 
-__version__ = "0.5.118"
+__version__ = "0.5.119"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHONPATH_SRC = {"PYTHONPATH": "src"}
