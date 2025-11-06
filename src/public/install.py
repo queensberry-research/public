0204267e25da724e824744d9f8667416014ded80
Derek Wan <d.wan@icloud.com>
2025-11-06 20:32:18 +0900

    Add `version2` to `README.md` (#392)

--- src/public/install.py
+++ src/public/install.py
@@ -34,7 +34,7 @@ basicConfig(
 _LOGGER = getLogger(__name__)
 
 
-__version__ = "0.5.121"
+__version__ = "0.5.122"
 _HOME_PUBLIC = Path("~/public").expanduser()
 _HOME_INFRA = Path("~/infra").expanduser()
 _PYTHONPATH_SRC = {"PYTHONPATH": "src"}
