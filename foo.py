0f540073b5c41c29b52c85920cb17e2f0f3581fb
Derek Wan <d.wan@icloud.com>
2025-11-08 13:47:14 +0900

    Remove `foo.py` (#427)

--- foo.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-lines = ["a", "b", "c", "d", "e", "f"]
-n = 6
-for i in range(0, n, 2):
-    left = i
-    right = i + 1
--- install.py
+++ install.py
@@ -28,7 +28,7 @@ basicConfig(
 )
 _LOGGER = getLogger(__name__)
 __all__ = ["SUBNETS", "BaseOperator", "PathLike", "Subnet", "run"]
-__version__ = "0.6.13"
+__version__ = "0.6.14"
 
 
 # types
